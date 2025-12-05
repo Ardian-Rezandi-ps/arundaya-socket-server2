2555d88f48ca72e3a68c8230cc3c594bed49f7b0
Ardian Rezandi <85152072+Ardian-Rezandi-ps@users.noreply.github.com>
2025-12-05 09:44:47 +0700

    fix eror lokal 3000

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const { Server } = require("socket.io");
 const app = express();
 const server = http.createServer(app);
 
-// PORT dari Railway, fallback ke 3000 saat lokal
 const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
@@ -15,30 +14,15 @@ const io = new Server(server, {
   },
 });
 
-// endpoint HTTP untuk tes di browser
 app.get("/", (req, res) => {
   res.send("Arundaya Socket Server2 is running ✅");
 });
 
-// event Socket.IO
 io.on("connection", (socket) => {
   console.log("Client connected:", socket.id);
 
-  socket.on("join-controller", (data) => {
-    console.log("join-controller", data);
-    io.emit("join-controller", data);
-  });
-
   socket.on("joystick-move", (data) => {
     io.emit("joystick-move", data);
-  });
-
-  socket.on("controller-status", (data) => {
-    io.emit("controller-status", data);
-  });
-
-  socket.on("controller-chat", (data) => {
-    io.emit("controller-chat", data);
   });
 
   socket.on("disconnect", () => {
@@ -47,5 +31,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(PORT, () => {
-  console.log("Socket.IO server running on port", PORT);
+  console.log("Server running on port " + PORT);
 });
